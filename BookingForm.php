bd1e74c139a2fd63a4692caaca386b12e55f5009
hg651460 <harshaghaturle@gmail.com>
2017-12-02 12:46:14 -0500

    booking form fixed html part

--- BookingForm.php
+++ BookingForm.php
@@ -1,85 +1,102 @@
-<html>
-	<head>
-<style>
-div.container {
-    width: 100%;
-  
-}
-header, footer {
-    padding: 1em;
-    color: LightCoral;
-    background-color: pink;
-    clear: left;
-    text-align: center;
-	background: url(header.jpg);
-	background-position: 100% 100%;
-    
-}
-article {
-   
-    padding: 1em;
-    overflow: hidden;
-    color:PaleVioletRed;
-	text-align: left;
-    
-}
-/* Full-width input fields */
-input[type=text], input[type=password] {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
-}
-/* Set a style for all buttons */
-.button {
-    background-image: #ffffff;
-    color: #C71585;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: #C71585;
-    cursor: pointer;
-    width: 40%;
-text-align:center;
-}
-/* Float cancel and signup buttons and add an equal width */
-.cancelbtn,.signupbtn {
-    float: left;
-    width: 50%;
-}
-/* Add padding to container elements */
-.container {
-    padding: 16px;
-}
-/* Clear floats */
-.clearfix::after {
-    content: "";
-    clear: both;
-    display: table;
-}
-</style>
+
+    <?php
+    include('session.php');
+    ?>
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+
+    <style>
+    div.container {
+        width: 100%;
+    }
+
+
+    header {
+        padding: 1em;
+        border-color: antiquewhite;
+        color: black;
+        background-color: pink;
+        clear: left;
+        font-size: 250%;
+        font-style: italic;
+        text-align:center;
+        text-decoration-color:black;
+        background: url(images/p.jpg);
+
+        
+    }
+        footer {
+        padding: 1em;
+        border-color: antiquewhite;
+        color: black
+        background-color: pink;
+        clear: left;
+        text-align:center;
+        text-decoration-color:black;
+        background: url(images/p.jpg);
+
+        
+    }
+    /* Set a style for all buttons */
+    .button {
+        background-image: #ffffff;
+        color: #C71585;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: #C71585;
+        cursor: pointer;
+        width: 40%;
+        font-size: medium;
+    text-align:center;
+    }
+
+
+    article {
+        margin-left: 170px;
+        border-left: 1px solid gray;
+        padding: 2em;
+        overflow: hidden;
+        color:PaleVioletRed;
+        font-size: x-large;
+        text-align: center;
+       
+    }
+    </style>
+
+
     </head>
     <body>
 
-<div class="container">
-<header>
-   <h1>Plan Your Party</h1>
-</header>
+    <div class="container">
 
+    <header>
+       <h1>MyParty</h1>
+    </header>
+      
 <article>
 
-        <form action= "bookthis.php" method="post">
-       
-        Number of Guest: <input type= "text" name="NoOfGuests"><br>
-        
-        Select a Date: <input type="date" name="Date" ><br><br>
-        
-        Select a Time: <input type="time" name="Time">
+        <form action= >
+       <table border="0">
+       <tr><td> Party type: </td><td><input type= "text" name="Type"></td></tr>
+          
+       <tr><td> Party theme: </td><td> <input type= "text" name="Theme"></td></tr>
 
-        Type: <select name="type">
+       <tr><td> Number of Guest: </td><td> <input type= "text" name="NoOfGuests"></td></tr>
+        
+       <tr><td> Select a Date: </td><td> <input type="date" name="Date" ></td></tr>
+        
+       <tr><td> Select a Time: </td><td> <input type="time" name="Time"></td></tr>
+        
+       <tr><td> Location: </td><td> <select name="place">
             <?
-            $sql="Select TypeName from type"
+            $sql="Select p.PlaceName 
+                    from Place p, party a
+                    Where p.placeID=a.placeID AND 
+                    a.date =! '' AND
+                    a.time =! ''
+                    "
             $q=mysql_query($sql)
             echo "<select name= 'type' >"; 
                 while($row = mysql_fetch_array($q)) {        
@@ -87,22 +104,14 @@ text-align:center;
                 }
             echo "</select>";
             ?>
-        
-        Theme:  <select name="theme">
-            <?
-            $sql="Select ThemeName from type"
-            $q=mysql_query($sql)
-            echo "<select name= 'theme' >"; 
-                while($row = mysql_fetch_array($q)) {        
-                echo "<option value='".$row['ThemeName']."'>".$row['ThemeName']."</option>"; 
-                }
-            echo "</select>";
-            ?>
+<!--          cross with time, date, place, and number of guest, so that it is availble
+            use party table -->
+       </td></tr></br>
+           </table>
         
         <div >
 		 <input class="button" type="submit" name="submit" value="Sumbit">
-		 </div>
-        <div >
+		
 		 <input class="button" type="reset" name="reset" value="Reset">
 		 </div>
         </form>
@@ -113,6 +122,6 @@ text-align:center;
 
 </div>
 
-    </body>
+</body>
 
 </html>
