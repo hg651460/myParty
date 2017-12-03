ee4eb44b200e3f9132aa8f002c7cb62be259c9ee
BatoolAbid <tola_abid@yahoo.com>
2017-12-03 02:41:03 -0500

    Update Signup.php
    Add fields validation (html form side), before data goes into the database

--- Signup.php
+++ Signup.php
@@ -3,29 +3,130 @@
 
 ?>
 
+
+
 <!DOCTYPE html>
 <html>
 <head>
+    
+      <script >
+     function validateForm()
+     {
+        
+		
+	   //********************************************************
+	   
+	   //validate password   ( OK  )
+		var pass=document.getElementById('password');
+        var passw=  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,20}$/;
+        var isEmail = document.getElementById('email')
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+         
+		if (pass.value.length == 0) 
+		{
+	    document.getElementById('passwordError').innerHTML = "  no password entered !";
+		return false;
+		}
+        else if(pass.value.match(passw))   
+        {   
+        return true;  
+        }  
+        else  
+        { 
+		document.getElementById('passwordError').innerHTML = "  not a valid password  !";  
+        alert("wrong password , it must be with minimum of 6 characters and at least one number, and one upper case characters, and one special character"); 
+        return false;  
+        } 
+		//return true;
+		//end password alidation 
+	
+	   //**************************************************
+	   
+		//validate email ( NOT OK)
+    	
+       /*
+       
+       
+       //validate email 
+    	var isEmail = document.getElementById('email')
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+    
+        else if (isEmail.value.length == 0) 
+		{
+	    document.getElementById('EmailError').innerHTML = "  no email entered !";
+		return false;
+		}
+        else if(isEmail.value.match(emailReg))
+		{
+
+		return true;
+        }
+		
+        else
+		{
+	    document.getElementById('EmailError').innerHTML = "not a valid email";
+        //alert("no valid email !");
+		return true;
+        }
+		//end email validation 
+        
+       */
+	
+	return true;
+	}//end outer function
+        
+   </script>
+    
 <style>
 div.container {
     width: 100%;
   
 }
 
-header, footer {
+header {
     padding: 1em;
-    color: LightCoral;
+    color:darkmagenta;
     background-color: pink;
     clear: left;
-    text-align: center;
+    font-size: 250%;
+    text-align:justify;
+    text-decoration-color:black;
+	background: url(p.jpg);
+    background-position: center;
+    
+}
+    footer {
+    padding: 1em;
+    border-color: antiquewhite;
+    color:darkmagenta;
+    clear: left;
+    text-align:center;
+    text-decoration-color:black;
 	background: url(header.jpg);
-	background-position: 100% 100%;
+
     
 }
 
 
+nav {
+    float: left;
+    width: 110px;
+    margin: 0;
+    padding: 1em;
+   
+}
 
-
+nav ul {
+    list-style-type:none;
+    padding: 0;
+    
+}
+   
+nav ul a {
+    text-decoration:none;
+     
+}
 article {
    
     padding: 1em;
@@ -36,7 +137,7 @@ article {
 }
 /* Full-width input fields */
 input[type=text], input[type=password] {
-    width: 100%;
+    width: 50%;
     padding: 12px 20px;
     margin: 8px 0;
     display: inline-block;
@@ -53,7 +154,7 @@ input[type=text], input[type=password] {
     border: #C71585;
     cursor: pointer;
     width: 40%;
-text-align:center;
+    text-align:center;
 }
 
 
@@ -78,44 +179,61 @@ text-align:center;
 </style>
 
 
+    <!-- ----------------------------- -->
 
 </head>
 <body>
-
+   
+   
+    
 <div class="container">
 <header>
    <h1>MyParty</h1>
 </header>
-
+<nav>
+  <ul>
+    <li><a href="Home.php">Home</a></li>
+    <li><a href="partyblogs.htm">Gallery</a></li>
+    <li><a href="contact.html">Contact</a></li>
+  </ul>
+</nav>
+    
+<p><font face="verdana" color= "#cc6699" size"6">Want to plan a party ? We can help! Simply fill the form below and we will find you a planner.. </font> </p>
 <article>
-<form id="signup_form" action="#" method="POST" enctype="multipart/form-data">
+
+<form id="signup_form" action="signupAction.php"  onsubmit="return validateForm();"  method="POST"  >
 
 		<label for="First name">First name:</label><br />
-		<input id="Fname" type="text" placeholder="First Name" class="input" name="Fname"  required>
+		<input id="Fname" type="text" placeholder="First Name" class="input" name="Fname" required ><br />
 	
 		<label for="Last name">Last name:</label><br />
-		<input id="Lname" placeholder="First Name" class="input" name="Lname" type="text" value="" required>
+		<input id="Lname" placeholder="Last Name" class="input" name="Lname" type="text" value="" required><br />
     
         <label for="phone">phone number:</label><br />
-		<input id="phone" placeholder="phone number" class="input" name="phone" type="text" value="" required>
+		<input id="phone" class="input" name="phone" type="text" value=""placeholder="0000000000" maxlength="10" required><br />
 	
 		<label for="email">Email:</label><br />
-		<input id="email" placeholder="Email" class="input" name="email" type="text" value="" required>
-		<label>Password</label>
-		<input type="password" placeholder="Enter Password" name="psw" required>
-		<label>Repeat Password</label>
-    	<input type="password" placeholder="Repeate Password" name="psw" required>
+		<input id="email" placeholder="example@example.com" class="input" name="email" type="text" value="" required ><span id="EmailError"></span><br />
+		<label>Password</label><br />
+		<input type="password" placeholder="Enter Password" id="password" name="psw" required><span id="passwordError"> </span>  
+    
 		
 		<div >
-		 <input class="button" type="submit" name="submit" value="Sign Up">
-		 </div>
+		 <input class="button"   type="submit" name="submit" value="Sign Up" >
+        </div>
 
-     
-</div>
-</form>						
+     <!-- --------------------------------- -->
+    </form>  
+
+ 
 </article>
 
-<footer>Copyright &copy; MyParty.com </footer>
+<footer> <script type="text/javascript">
+        
+        var today = new Date();
+        document.writeln( today.getFullYear() );
+    
+      </script>Copyright &copy; MyParty.com </footer>
 
 </div>
 
